75fdf32f391193df924cb9ecf066a16c1be90dbc
Yassine Karimi <yassinkarimi.1.23@gmail.com>
2023-10-16 13:29:04 +0100

    1s commit

---
+++ src/main/java/org/sid/democloud_kafka/DemoCloudKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.sid.democloud_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCloudKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/democloud_kafka/DemoCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.democloud_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
